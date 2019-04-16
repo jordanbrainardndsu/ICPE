3d88ea954a4ba3de52dbe3752bbd256eec10fb75
jordanbrainardndsu <47485003+jordanbrainardndsu@users.noreply.github.com>
2019-04-15 19:10:42 -0500

    Add files via upload
    Array of countries to help with coloring map

---
+++ country_colors.java
@@ -0,0 +1,40 @@
+/**
+ * @author Emily Budke
+ * 
+ * Separates world countries into four colors so that no adjacent countries will
+ * have the same color on a map. 
+ * 
+ */
+class Colors {
+    
+    String[] red = {"Albania", "Argentina", "Australia", "Azerbaijan", "Belarus",
+        "Bhutan", "Bosnia and Gerzegovina", "Bulgaria", "Cyprus", "Democratic Republic of the Congo",
+        "El Salvador", "Finland", "Gabon", "Gambia", "Germany", "Guyana", "Hungary", 
+        "Iceland", "Iraq", "Italy", "Jamaica", "Kazakhstan", "Laos", "Liberia", "Malawi",
+        "Malaysia", "Mauritania", "Mexico", "Niger", "North Korea", "Pakistan", "Peru", 
+        "Somalia", "Spain", "Sri Lanka", "Sudan", "Swaziland", "Taiwan", "Tajikistan", 
+        "The Bahamas", "Togo", "United Arab Emirates", "Vanuatu", "Yemen"}; 
+    
+    String[] blue = {"Angola", "Bangladesh", "Belize", "Bolivia", "Botswana", "Burkina Faso", 
+        "Burundi", "Central African Republic", "China", "Costa Rica", "Czech Republic", "Estonia",
+        "France", "Georgia", "Greece", "Guinea", "Indonesia", "Iran", "Ireland", "Israel", 
+        "Kosovo", "Lebanon", "Libya", "Lithuania", "Mozambique", "Nigeria", "Norway", "Oman", 
+        "Puerto Rico", "Slovenia", "Thailand", "Uganda", "Ukraine",  "United States", 
+        "Uzbekistan", "Venezuela", "Western Sahara", "New Caledonia"};
+  
+    String[] yellow = {"Algeria", "Armenia", "Benin", "Cameroon", "Cananda", "Colombia", 
+        "Cuba", "Djibouti", "Dominican Republic", "Guatemala", "India", "Ivory Coast", 
+        "Kyrgyzstan", "Lesotho", "Macedonia", "Madegascar", "Moldova", "Montenegro", "Namibia",
+        "Netherlands", "New Zealanad", "Nicaragua", "Paraguay", "Portugal", "Russia",
+        "Saudi Arabia", "Senegal", "Slovakia", "Solomon Islands", "South Korea", "South Sudan", 
+        "Suriname", "Sweden", "Switzerland", "Syria", "Turkmenistan", "United Kingdom", 
+        "United Republic of Tanzania", "Uruguay", "Vietnam"};
+    
+    String[] green = {"Afghanistan", "Austria", "Belgium", "Brazil", "Brunei", "Cambodia", "Chad", 
+        "Chile", "Congo", "Croatia", "Denmark", "Ecuador","Egypt", "Equatorial Guinea","Eritrea", 
+        "Fiji", "Ghana", "Greenland", "Guinea Bissau", "Haiti", "Honduras", "Japan", "Jordan",
+        "Kenya", "Kuwait", "Latvia", "Mali", "Mongolia", "Morocco", "Myanmar", "Nepal", 
+        "Panama", "Papua New Guinea", "Philippines", "Poland", "Qatar", "Romania", "Rwanda",
+        "Sierra Leone", "South Africa","Trinidad and Tobago", "Tunisia", "Turkey", "Zambia"};
+    
+}
